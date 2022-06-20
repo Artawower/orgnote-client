6db95e7d6f58029283d5aa290435b183c686f477
artawower <artawower@protonmail.com>
2022-06-20 23:25:11 +0400

    feature: add note footer

---
+++ src/components/NoteFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="footer">
+    <slot></slot>
+    <!-- TODO: master  shared icons here -->
+  </div>
+</template>
+
+<script langh="ts" setup></script>
+
+<style lang="scss"></style>
--- src/pages/NoteView.vue
+++ src/pages/NoteView.vue
@@ -1,20 +1,32 @@
 <template>
   <q-page padding>
     <h1>{{ selectedNote?.meta.title }}</h1>
+    <h4 class="note-description">{{ selectedNote?.meta.description }}</h4>
     <content-renderer :content="selectedNote?.content"></content-renderer>
+    <note-footer>
+      <div v-if="selectedNote?.meta?.tags" class="tags-wrapper">
+        <q-badge
+          v-for="tag in selectedNote?.meta?.tags"
+          :key="tag"
+          @click="searchByTag(tag)"
+          rounded
+          outline
+          color="primary"
+          class="tag"
+          :label="tag"
+        />
+      </div>
+    </note-footer>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { defineComponent, watch } from 'vue';
+import { watch } from 'vue';
 import { useRoute } from 'vue-router';
 import { useNotesStore } from 'stores/notes';
 import { storeToRefs } from 'pinia';
 import ContentRenderer from 'components/ContentRenderer.vue';
-
-defineComponent({
-  ContentRenderer,
-});
+import NoteFooter from 'components/NoteFooter.vue';
 
 const noteStore = useNotesStore();
 const route = useRoute();
@@ -31,6 +43,23 @@ watch(
     noteStore.selectNoteById(id as string);
   }
 );
+
+const searchByTag = (tag: string) => {
+  console.log(tag);
+  throw new Error('Method not implemented.');
+};
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.note-description {
+  color: $smog;
+  /* TODO: master  choose font for cursive*/
+  font-style: italic;
+}
+.tag {
+  cursor: pointer;
+  &:not(:first-child) {
+    margin-left: 0.5rem;
+  }
+}
+</style>
