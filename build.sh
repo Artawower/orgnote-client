9f6f3d362af0ce9a99f2e8031b06b3e2d651e352
artawower <artawower@mail.ru>
2023-06-16 00:32:00 +0300

    fix(ci): update build script for android app sharing

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 yarn build:all
-java -jar bundletool.jar build-apks --bundle=./dist/cordova/android/bundle/release/app-release.aab --output=./dist/second-brain.apks
+mkdir ./dist/build
+java -jar bundletool.jar build-apks --bundle=./dist/cordova/android/bundle/release/app-release.aab --output=./dist/build/second-brain.apks
