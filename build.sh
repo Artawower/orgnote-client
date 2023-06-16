1dd4e30f4063f8136208642353486156ba3c4d97
artawower <artawower@mail.ru>
2023-06-16 22:23:42 +0300

    fix(ci): android build link

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 yarn build:all
 mkdir ./dist/build
-java -jar bundletool.jar build-apks --bundle=./dist/cordova/android/bundle/release/app-release.aab --output=./dist/builds/second-brain.apks
+java -jar bundletool.jar build-apks --bundle=./dist/cordova/android/bundle/release/app-release.aab --output=./dist/pwa/builds/second-brain.apks
