44f3bbcf6ca9f8d1e7cca27355bb9416ed0dfa0e
artawower <artawower@mail.ru>
2023-07-16 02:30:48 +0300

    refactor(pinia): migrate view store to composition api

--- src/stores/view.ts
+++ src/stores/view.ts
@@ -1,32 +1,24 @@
 import { defineStore } from 'pinia';
+import { computed, ref } from 'vue';
 
-interface ViewState {
-  tile?: boolean;
-  loadingCount: number;
-}
+export const useViewStore = defineStore('view', () => {
+  const tile = ref<boolean>(false);
+  const loadingCount = ref<number>(0);
 
-const defaultState: ViewState = {
-  tile: false,
-  loadingCount: 0,
-};
+  const hasGlobalLoading = computed(() => loadingCount.value > 0);
 
-export const useViewStore = defineStore('view', {
-  state: (): ViewState => defaultState,
-  getters: {
-    hasGlobalLoading(): boolean {
-      return this.loadingCount > 0;
-    },
-  },
-  actions: {
-    toggleTile(): void {
-      this.tile = !this.tile;
-    },
-    addLoading(): void {
-      this.loadingCount += 1;
-    },
-    removeLoading(): void {
-      this.loadingCount -= 1;
-    },
-  },
-  persist: true,
+  const toggleTile = () => (tile.value = !tile.value);
+  const addLoading = () => (loadingCount.value += 1);
+  const removeLoading = () => (loadingCount.value -= 1);
+
+  return {
+    tile,
+    loadingCount,
+
+    hasGlobalLoading,
+
+    toggleTile,
+    addLoading,
+    removeLoading,
+  };
 });
