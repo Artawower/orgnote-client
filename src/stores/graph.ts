af9145efd8695058bd0f324f4957e42b963bce27
artawower <artawower@mail.ru>
2023-07-16 02:32:36 +0300

    refactor(pinia): migrate graph state to composition api

--- src/stores/graph.ts
+++ src/stores/graph.ts
@@ -1,28 +1,26 @@
 import { defineStore } from 'pinia';
 import { sdk } from 'src/boot/axios';
 import { ModelsNoteGraph } from 'src/generated/api';
+import { ref } from 'vue';
 
-interface GraphState {
-  // TODO: add correct type for graph
-  graph: ModelsNoteGraph;
-  loading: boolean;
-  error?: Error;
-}
+export const useGraphStore = defineStore('graph', () => {
+  const graph = ref<ModelsNoteGraph>(null);
+  const loading = ref<boolean>(false);
+  const error = ref<Error>(null);
 
-export const useGraphStore = defineStore('graph', {
-  state: (): GraphState => ({
-    graph: null,
-    loading: false,
-    error: null,
-  }),
-  actions: {
-    async loadGraph() {
-      try {
-        this.graph = (await sdk.notes.notesGraphGet()).data.data;
-      } catch (e) {
-        // TODO: handle error
-        console.warn(e);
-      }
-    },
-  },
+  const loadGraph = async () => {
+    try {
+      graph.value = (await sdk.notes.notesGraphGet()).data.data;
+    } catch (e) {
+      // TODO: handle real error [low]
+      console.warn(e);
+    }
+  };
+
+  return {
+    graph,
+    loading,
+    error,
+    loadGraph,
+  };
 });
