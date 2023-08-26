fc48c27e7f5565ab6cbcfb7d0fea44b1250674b3
artawower <artawower@mail.ru>
2023-08-26 17:18:18 +0400

    fix: code highlight after webpack treeshake

--- src/boot/highlightjs.ts
+++ src/boot/highlightjs.ts
@@ -1,9 +1,11 @@
-import 'highlight.js/lib/common';
-
 import { boot } from 'quasar/wrappers';
 import hljsVuePlugin from '@highlightjs/vue-plugin';
-import 'highlight.js';
+import hljsCommon from 'highlight.js/lib/common';
 
 export default boot(({ app }) => {
+  // NOTE: https://github.com/highlightjs/vue-plugin/issues/44#issuecomment-1594686145
+  hljsCommon.highlightAuto(
+    '<h1>Highlight.js has been registered successfully!</h1>'
+  ).value;
   app.use(hljsVuePlugin);
 });
