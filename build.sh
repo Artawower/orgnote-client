988ee384988bcc28758ccac4243e3ca9c8dcde84
artawower <artawower@mail.ru>
2023-08-27 14:37:46 +0400

    fix(ci): integrate keystore and signing into android build

--- build.sh
+++ build.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-yarn build:all
-mkdir ./dist/build
-java -jar bundletool.jar build-apks --mode=universal --bundle=./dist/cordova/android/bundle/release/app-release.aab --output=./dist/pwa/builds/second-brain.apks
+# yarn build:all
+# mkdir ./dist/build
+
+# password=$(uuidgen)
+# echo y | keytool -genkeypair \
+#                  -dname "cn=Artur Iaroshenko, c=GE" \
+#                  -alias sb \
+#                  -keypass $password \
+#                  -keystore ./sb-release.keystore \
+#                  -storepass $password \
+#                  -keyalg RSA \
+#                  -keysize 4096 \
+#                  -validity 20000
+
+echo $KEYPASS
+echo $STOREPASS
+
+java -jar bundletool.jar build-apks \
+     --mode=universal \
+     --bundle=./dist/cordova/android/bundle/release/app-release.aab \
+     --output=./dist/pwa/builds/second-brain.apks \
+     --ks=./sb-release.keystore \
+     --ks-pass=pass:$KEYPASS \
+     --ks-key-alias=sb \
+     --key-pass=pass:$STOREPASS
+
 unzip -p ./dist/pwa/builds/second-brain.apks universal.apk > ./dist/pwa/builds/second-brain.apk
