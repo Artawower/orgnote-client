05340fb42fa859a46a92182141eec79e819f4fb3
artawower <artawower@mail.ru>
2023-09-28 18:56:17 +0400

    refactor: expose editor languages from raw editor

--- src/components/containers/raw-editor/RawEditor.vue
+++ src/components/containers/raw-editor/RawEditor.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script lang="ts" setup>
+import { editorLanguages } from './editor-languages';
 import { closeBrackets } from '@codemirror/autocomplete';
 import { bracketMatching, indentOnInput } from '@codemirror/language';
 import { EditorState } from '@codemirror/state';
@@ -14,54 +15,14 @@ import {
   highlightActiveLine,
   rectangularSelection,
 } from '@codemirror/view';
-import {
-  LanguageName,
-  langs,
-  loadLanguage,
-} from '@uiw/codemirror-extensions-langs';
 import { minimalSetup } from 'codemirror';
-import { NodeType, OrgNode, parse, walkTree } from 'org-mode-ast';
+import { OrgNode } from 'org-mode-ast';
 import { OrgUpdatedEffect, orgMode } from 'src/tools/cm-org-language';
 import { newOrgModeDecorationPlugin } from 'src/tools/cm-org-language/widgets';
-import {
-  addTableEffect,
-  newTableField,
-} from 'src/tools/cm-org-language/widgets';
+import { newTableField } from 'src/tools/cm-org-language/widgets';
 import { orgMultilineWidgets } from 'src/tools/cm-org-language/widgets/multiline-widgets';
 
 import { getCurrentInstance, onMounted, ref, toRef, watch } from 'vue';
-
-// TODO: master refactor. Move editor definition to separate file
-const supportedLanguages: LanguageName[] = [
-  'angular',
-  'c',
-  'clojure',
-  'commonLisp',
-  'cpp',
-  'css',
-  'dart',
-  'go',
-  'java',
-  'javascript',
-  'json',
-  'kotlin',
-  'mysql',
-  'php',
-  'python',
-  'sass',
-  'sql',
-  'shell',
-  'dockerfile',
-  'r',
-  'toml',
-  'ruby',
-  'rust',
-  'html',
-  'csharp',
-  'scala',
-];
-
-supportedLanguages.forEach((lang) => loadLanguage(lang));
 
 const props = withDefaults(
   defineProps<{
@@ -104,40 +65,7 @@ const initEditor = () => {
             effects: OrgUpdatedEffect.of(updatedOrgNode),
           });
         },
-        wrap: {
-          'web-mode': langs.vue().language.parser,
-          java: langs.java().language.parser,
-          'c++': langs.cpp().language.parser,
-          c: langs.cpp().language.parser,
-          cpp: langs.cpp().language.parser,
-          rust: langs.rust().language.parser,
-          javascript: langs.javascript().language.parser,
-          js: langs.javascript().language.parser,
-          typescript: langs.typescript().language.parser,
-          ts: langs.typescript().language.parser,
-          json: langs.json().language.parser,
-          clojure: langs.clojure().language.parser,
-          'common-lisp': langs.commonLisp().parser,
-          css: langs.css().language.parser,
-          dart: langs.dart().parser,
-          go: langs.go().parser,
-          html: langs.html().language.parser,
-          kotlin: langs.kotlin().parser,
-          mysql: langs.mysql().language.parser,
-          csharp: langs.csharp().language.parser,
-          r: langs.r().parser,
-          ruby: langs.ruby().parser,
-          sass: langs.sass().language.parser,
-          scala: langs.scala().parser,
-          shell: langs.shell().parser,
-          sql: langs.sql().language.parser,
-          toml: langs.toml().parser,
-          dockerfile: langs.dockerfile().parser,
-          php: langs.php().language.parser,
-          python: langs.python().language.parser,
-          angular: langs.angular().language.parser,
-          vue: langs.vue().language.parser,
-        },
+        wrap: editorLanguages,
       }),
       highlightActiveLine(),
       minimalSetup,
