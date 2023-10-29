a2ea97ea9251b54ee9c089c777beea614900b5d2
artawower <artawower@mail.ru>
2023-10-29 15:45:35 +0400

    fix: icon for github auth

--- src/components/LoginButtons.vue
+++ src/components/LoginButtons.vue
@@ -3,14 +3,14 @@
     class="q-gutter-sm login-button flex flex-center"
     :class="vertical ? 'q-py-md' : 'q-pa-md'"
   >
-    <q-btn @click="authGithub" round flat outline>
-      <div class="github-btn btn-iconed"></div>
+    <q-btn @click="authGithub" round flat outline icon="fa-brands fa-github">
     </q-btn>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useAuthStore } from 'src/stores/auth';
+
 import { ref } from 'vue';
 
 const authStore = useAuthStore();
@@ -25,12 +25,3 @@ const authGithub = () => {
   authStore.authViaGithub();
 };
 </script>
-
-<style lang="scss">
-.github-btn {
-  width: var(--btn-social-size);
-  height: var(--btn-social-size);
-  mask-image: url('/icons/github.svg');
-  mask-size: cover;
-}
-</style>
