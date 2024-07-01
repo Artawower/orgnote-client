369165ef570b450ad6681bafff66a0e69c38a601
Art <Artawower@mail.ru>
2024-07-01 17:52:27 +0400

    feat: status bar configs. Support for sidebar

--- src-capacitor/www/assets/min-page-height-C8LjVHEA.js
+++
@@ -1,1 +0,0 @@
-import{m as t}from"./index-D7E76tEZ.js";function i(){const r=t();if(!(r&&r.platform.is.desktop))return{minHeight:"0"}}export{i as r};
--- src-capacitor/www/assets/use-hydration-DlVtJJeb.js
+++
@@ -1,1 +0,0 @@
-import{r as e,ax as r,ai as t}from"./index-D7E76tEZ.js";function u(){const a=e(!r.value);return a.value===!1&&t(()=>{a.value=!0}),{isHydrated:a}}export{u};
