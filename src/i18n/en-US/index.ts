e39dd8457dc5c47ac85996fa573bc502325ccd0a
Art <Artawower@mail.ru>
2025-01-08 14:49:59 +0530

    refactor: i18n with keys

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -1,7 +1,14 @@
 // This is just an example,
 // so you can safely delete all default props below
 
-export default {
-  failed: 'Action failed',
-  success: 'Action was successful'
+import type { I18NKeys } from 'orgnote-api';
+
+const eng: { [key in I18NKeys]: string } = {
+  loadingMessage1: 'loading...',
+  loadingMessage2: 'fetching data...',
+  loadingMessage3: 'almost there...',
+  loadingMessage4: 'compiling magic...',
+  loadingMessage5: 'just a moment...',
 };
+
+export default eng;
