9b0cb3063564de4f2a303ed5af931b05fe7dbe20
Artur Iaroshenko <darkawower@Arturs-MacBook-Pro.local>
2026-01-29 17:41:59 +0400

    fix: correct quotes

--- src/extensions/org-quote-block/OrgQuoteBlock.vue
+++ src/extensions/org-quote-block/OrgQuoteBlock.vue
@@ -1,9 +1,11 @@
 <template>
-  <blockquote class="org-quote">
-    <p v-for="(child, i) in quoteChildren" :key="i">
-      <content-renderer :node="child" />
-    </p>
-  </blockquote>
+  <div class="org-quote-wrapper">
+    <blockquote class="org-quote">
+      <p v-for="(child, i) in quoteChildren" :key="i">
+        <content-renderer :node="child" />
+      </p>
+    </blockquote>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -17,14 +19,18 @@ const props = defineProps<{
 }>();
 
 const currentNode = computed(() => props.nodeGetter?.() ?? props.node);
-const quoteChildren = computed(() => currentNode.value?.children ?? []);
+const quoteChildren = computed(() => currentNode.value?.children?.get(2)?.children ?? []);
 </script>
 
 <style scoped lang="scss">
+.org-quote-wrapper {
+  padding-left: var(--editor-line-left-padding);
+}
+
 .org-quote {
   display: block;
   border-left: 3px solid var(--accent);
-  padding-left: var(--padding-md);
+  padding-left: var(--padding-lg);
   margin: 0;
   font-style: italic;
 
