377eb8aebdaf2b833ba1b8e3719efe895faed937
Artur Iaroshenko <artawower@protonmail.com>
2026-04-08 17:13:15 +0400

    fix(pwa): enable SSR production navigation fallback

--- src-pwa/custom-service-worker.ts
+++ src-pwa/custom-service-worker.ts
@@ -50,7 +50,7 @@ registerRoute(
   })
 );
 
-if (process.env.MODE !== 'ssr' && process.env.PROD) {
+if (process.env.PROD) {
   registerRoute(
     new NavigationRoute(createHandlerBoundToURL(process.env.PWA_FALLBACK_HTML), {
       denylist: [
