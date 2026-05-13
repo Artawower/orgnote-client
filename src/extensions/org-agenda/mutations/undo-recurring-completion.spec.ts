aecbf768aa989725f4369c376f6ba4a21f5a72db
Artur Iaroshenko <artawower@protonmail.com>
2026-05-14 03:05:58 +0400

    feat(org-agenda): undo recurring completion via LOGBOOK entry removal

---
+++ src/extensions/org-agenda/mutations/undo-recurring-completion.spec.ts
@@ -0,0 +1,13 @@
+import { expect, test } from 'vitest';
+import { undoRecurringCompletion } from './undo-recurring-completion';
+
+test('undoRecurringCompletion_removesTodayDoneEntry', () => {
+  const content =
+    '* TODO Daily\nSCHEDULED: <2026-05-14 Thu +1d>\n:LOGBOOK:\n- State "DONE" from "TODO" [2026-05-13 Wed 14:30]\n:END:\nBody\n';
+
+  const result = undoRecurringCompletion(content, 0, '2026-05-13');
+
+  expect(result).toBeDefined();
+  expect(result).not.toContain('State "DONE"');
+  expect(result).toContain('SCHEDULED: <2026-05-14');
+});
