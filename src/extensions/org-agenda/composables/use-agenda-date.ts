a21c20c21772ad71e789b223197a51248aec630c
Artur Iaroshenko <artawower@protonmail.com>
2026-05-14 22:34:39 +0400

    fix(org-agenda): i18n for Today/Tomorrow date labels

--- src/extensions/org-agenda/composables/use-agenda-date.ts
+++ src/extensions/org-agenda/composables/use-agenda-date.ts
@@ -1,3 +1,5 @@
+import { useI18n } from 'vue-i18n';
+import { extensionI18nKeys } from 'src/constants/extension-i18n-keys';
 import { usePrettyDate } from 'src/composables/use-pretty-date';
 
 const parseOrgDateStr = (raw: string): Date =>
@@ -8,6 +10,8 @@ const isSameDay = (a: Date, b: Date): boolean =>
   a.getMonth() === b.getMonth() &&
   a.getDate() === b.getDate();
 
+const todayDate = (): Date => new Date();
+
 const tomorrowDate = (): Date => {
   const d = new Date();
   d.setDate(d.getDate() + 1);
@@ -15,13 +19,15 @@ const tomorrowDate = (): Date => {
 };
 
 export const useAgendaDate = () => {
+  const { t } = useI18n();
   const { prettyDate } = usePrettyDate();
 
   const prettyAgendaDate = (raw: string): string => {
     if (!raw) return '';
     const date = parseOrgDateStr(raw);
     if (Number.isNaN(date.getTime())) return '';
-    if (isSameDay(date, tomorrowDate())) return 'Tomorrow';
+    if (isSameDay(date, tomorrowDate())) return t(extensionI18nKeys.orgAgendaFilterTomorrow);
+    if (isSameDay(date, todayDate())) return t(extensionI18nKeys.orgAgendaFilterToday);
     return prettyDate(date);
   };
 
